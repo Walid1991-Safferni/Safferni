c1d446de5d35bd50cc2f5fdbbb1dc10d76f21e97
Walid1991-Safferni <m.walid.alokhwan@gmail.com>
2026-04-24 22:54:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1219,7 +1219,6 @@ export default function App(){
       </a>
     </div>
   );
-}
 
 const lbl={display:"block",fontSize:12,fontWeight:700,color:"#666",marginBottom:6};
 const inp={width:"100%",padding:"11px 14px",border:"1.5px solid #E0DDD8",borderRadius:10,background:"#FAFAF8",outline:"none",transition:"border-color 0.2s"};
